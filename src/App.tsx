b0588fecc14a4be88d4083bc72314994e777e206
saulpalv <19735362+saulpalv@users.noreply.github.com>
2021-12-31 10:02:59 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+
+export const App = () => {
+
+  return (
+    <span>Hello World</span>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+// @ts-check
+/** @typedef { import('webpack').Configuration } Configuration*/
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+/** @returns { Configuration } */
+module.exports = (env, argv) => {
+    const mode = argv.mode
+    const isDevelopment = mode === "development"
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        entry: './src/index.tsx',
+        devtool: 'inline-source-map',
+        devServer: {
+            hot: true,
+            static: './dist',
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "src/index.html",
+            }),
+            new ForkTsCheckerWebpackPlugin(),
+            isDevelopment && new ReactRefreshWebpackPlugin()
+
+        ].filter(Boolean),
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: require.resolve('ts-loader'),
+                            options: {
+                                transpileOnly: true,
+                            },
+                        },
+                    ]
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
+                }
+            ],
+        }
+    }
+}
